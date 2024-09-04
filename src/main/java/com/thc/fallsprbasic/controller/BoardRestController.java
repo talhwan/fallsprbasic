052844d8a7b5ad3cf5bee51eea6ca5e38903c7e1
01 <talhwan@hotmail.com>
2024-09-04 20:13:53 +0900

    board create in ctrl
    ctrl-, front-

---
+++ src/main/java/com/thc/fallsprbasic/controller/BoardRestController.java
@@ -0,0 +1,54 @@
+package com.thc.fallsprbasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/api/board")
+@RestController
+public class BoardRestController {
+    List<Map<String, Object>> boardList = new ArrayList<>();
+    @GetMapping("/create")
+    public Map<String, Object> create(@RequestParam Map<String, Object> params){
+        String title = (String) params.get("title");
+        String content = params.get("content") + "";
+        String author = params.get("author") + "";
+        int order = boardList.size();
+
+        Map<String, Object> boardMap = new HashMap<>();
+        boardMap.put("order", ++order);
+        boardMap.put("title", title);
+        boardMap.put("content", content);
+        boardMap.put("author", author);
+        boardList.add(boardMap);
+
+        System.out.println(boardList);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("resultCode", 200);
+
+        return resultMap;
+    }
+    @GetMapping("/list") //이 안에 있는 주소값은 꼭 유니크해야함!!
+    public List<Map<String, Object>> list(){
+        return boardList;
+    }
+    @GetMapping("/detail") //이 안에 있는 주소값은 꼭 유니크해야함!!
+    public Map<String, Object> detail(@RequestParam String order){
+
+        int index = Integer.parseInt(order) - 1;
+        Map<String, Object> boardMap = boardList.get(index);
+
+        Map<String, Object> resultMap = new HashMap<>();
+        resultMap.put("resultCode", 200);
+        resultMap.put("detail_board", boardMap);
+
+        return resultMap;
+    }
+}
---
+++ src/main/java/com/thc/fallsprbasic/controller/page/BoardController.java
@@ -0,0 +1,30 @@
+package com.thc.fallsprbasic.controller.page;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/board")
+@Controller
+public class BoardController {
+
+    /*
+    @GetMapping("/create")
+    public String create(){
+        return "board/create";
+    }
+    */
+
+    //PathVariable => url에 있는 값을 변수로 받아올수 있는 기능!!
+    @GetMapping("/{page}")
+    public String page(@PathVariable String page){
+        return "board/" + page;
+    }
+
+    @GetMapping("/{page}/{id}")
+    public String page2(@PathVariable String page, @PathVariable String id){
+        return "board/" + page;
+    }
+
+}
